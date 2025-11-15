ab22676997712736b80f68af808e80ffbcc59c8a
Montserrat Rodriguez <montse@Montserrats-MacBook-Pro.local>
2025-11-15 08:53:33 -0800

    Class 4

---
+++ class04/class4.r
@@ -0,0 +1,14 @@
+#' ---
+#' title: "Class 4"
+#' author: "Montserrat (A16536527)"
+#'
+#' ---
+#This is my first R script 
+x <- 1:50
+plot(x)
+plot(x,col="blue")
+#change color use col
+plot(x, sin(x), type = "l",col="red",lwd=3)
+log(10)
+log(10,base=10)
+log(10,2)
